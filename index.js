c44d1d617da5c6a3dd29ceaa83ca54516c14588d
GabrielaOlariu <olariugabriela@gmail.com>
2025-04-04 11:48:25 +0200

    Add input and button to JS

---
+++ index.js
@@ -0,0 +1,6 @@
+const inputFieldEl = document.getElementById("input-field");
+const buttonAddEl = document.getElementById("button-add");
+
+buttonAddEl.addEventListener("click", function() {
+    console.log("You clicked")
+})
