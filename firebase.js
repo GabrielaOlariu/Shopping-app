c558edf865abe135ea7e1099c26c4bef4d555cb6
GabrielaOlariu <olariugabriela@gmail.com>
2025-04-07 15:02:06 +0200

    Set up firedatabase

---
+++ firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAiKMuEL_C_D21h0TkSH8GiV1G12BbetxI",
+  authDomain: "shopping-cart-4ebfa.firebaseapp.com",
+  projectId: "shopping-cart-4ebfa",
+  storageBucket: "shopping-cart-4ebfa.firebasestorage.app",
+  messagingSenderId: "845628975496",
+  appId: "1:845628975496:web:9a622555509f23cacd723d",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+//https://shopping-cart-4ebfa-default-rtdb.europe-west1.firebasedatabase.app/
--- index.js
+++ index.js
@@ -1,6 +1,29 @@
+//Firebase field
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-app.js";
+
+import {
+  getDatabase,
+  ref,
+  push,
+  remove,
+} from "https://www.gstatic.com/firebasejs/11.6.0/firebase-database.js";
+
+const firebaseConfig = {
+  databaseURL:
+    "https://shopping-cart-4ebfa-default-rtdb.europe-west1.firebasedatabase.app/",
+};
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+const cartDB = ref(database, "gabrielaCart");
+
+//DOM Field
 const inputFieldEl = document.getElementById("input-field");
 const buttonAddEl = document.getElementById("button-add");
 
-buttonAddEl.addEventListener("click", function() {
-    console.log("You clicked")
-})
+buttonAddEl.addEventListener("click", function () {
+  let inputValue = inputFieldEl.value;
+  console.log(inputValue);
+  push(cartDB, inputValue);
+  inputFieldEl.value = "";
+});
